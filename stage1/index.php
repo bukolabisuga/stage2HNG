84924b33838506f9f1c332a6178a590a9ac48d74
bukola <bukola@wemove.co>
2018-04-12 09:00:37 +0100

    first commit

---
+++ stage1/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
+
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran" rel="stylesheet">
+
+    <title>HNG Internship Stage 1</title>
+</head>
+
+<body>
+    <div class="wrapper">
+
+        <div class="header">
+            <p class="title">HNG 4.0</p>
+            <p class="subtitle">Join Us For The Fourth Edition Of The Internship Now!!!</p>
+        </div>
+        <div class="info">
+            <p class="time"> <?php echo date("h:ia", time() - 3600); ?> </p>
+            <p class="date"> <?php echo date("Y-m-d"); ?> </p>
+        </div>
+
+    </div>
+</body>
+
+</html>
